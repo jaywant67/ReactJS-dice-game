d9ce8626eae75fcadf165c93384be609fdcfff02
Jaywant Chaudhary <147032549+jaywant67@users.noreply.github.com>
2024-11-10 16:00:42 +0530

    Add files via upload

---
+++ src/components/GamePlay.jsx
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+import NumberSelector from "./NumberSelector";
+import TotalScore from "./TotalScore";
+import RoleDice from "./RoleDice";
+import { useState } from "react";
+
+const GamePlay = () => {
+  const [score,setScore] = useState();
+  const [selectedNumber,setSelectedNumber] = useState();
+  const [currentDice, setCurrentDice] = useState(1);
+
+const generateRandomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min);
+  } 
+  
+  const roleDice = () => {
+    const randomNumber =  generateRandomNumber(1,7);
+
+    setCurrentDice((prev) => randomNumber);
+  
+  if (selectedNumber == randomNumber){
+    setScore((prev) => prev + randomNumber);
+}else
+  setScore((prev) => prev - 2);
+};
+
+
+
+  return (
+    <MainContainer>
+    <div className="top_section">
+        <TotalScore />
+        <NumberSelector  selectedNumber={selectedNumber} 
+          setSelectedNumber={setSelectedNumber}
+        />
+    </div>
+    <RoleDice 
+      currentDice={currentDice}
+      roleDice={roleDice}
+    />
+    </MainContainer>
+  )
+}
+
+export default GamePlay;
+
+const MainContainer = styled.main`
+padding-top: 70px;
+.top_section{
+    display: flex;
+    justify-content: space-around;
+    align-items: end;
+}
+`;
